e82a4f00804f3ed3c29b1d8c704e94e7b7982f28
TalShaf93 <73899987+TalShaf93@users.noreply.github.com>
2025-07-31 10:32:44 +0300

    refactor(worker): hide sidebar and update project table

--- src/layouts/WorkerLayout.jsx
+++ src/layouts/WorkerLayout.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import TopBar from '../components/TopBar';
-import Sidebar from '../components/Sidebar';
 import { cn } from '../utils/cn';
-import { SidebarProvider } from '../hooks/sidebar/SidebarProvider';
 import { TopbarProvider } from '../hooks/topbar/TopbarProvider';
 import { useAuth } from '../auth/useAuth';
 import { useNavigate, useLocation, Outlet } from 'react-router-dom';
@@ -21,27 +19,22 @@ const WorkerLayout = ({ className = '', ...props }) => {
   return (
     <div className={cn('min-h-screen bg-gray-50', className)} {...props}>
       <TopbarProvider>
-        <SidebarProvider>
-          <TopBar
-            onLogout={handleLogout}
-            onNavigate={handleNavigate}
-            currentPath={pathname}
-          />
-          <Sidebar onNavigate={handleNavigate} currentPath={pathname} />
+        <TopBar
+          onLogout={handleLogout}
+          onNavigate={handleNavigate}
+          currentPath={pathname}
+        />
+        <main
+          className={cn(
+            'transition-all duration-300',
+            'min-h-[calc(100vh-73px)]'
+          )}
+        >
+          <div className="p-6">
+            <Outlet />
+          </div>
 
-          <main
-            className={cn(
-              'transition-all duration-300',
-              'min-h-[calc(100vh-73px)]',
-              'lg:ml-64'
-            )}
-          >
-            <div className="p-6 max-w-4xl mx-auto">
-              <Outlet />
-            </div>
-
-          </main>
-        </SidebarProvider>
+        </main>
       </TopbarProvider>
     </div>
   );
